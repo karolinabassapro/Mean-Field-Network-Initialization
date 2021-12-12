6b082fe15014efa8d0f1130fdaa1b5c6960d1cce
Jason Rader <38091354+RaderJason@users.noreply.github.com>
2021-12-12 16:02:58 +0000

    Base linear thing from Teams meeting

---
+++ teams.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+class base_conv(nn.Module):
+    def __init__(self, num_in, num_classes, hidden_width, depth):
+        super().__init__()
+
+        self.depth = depth
+
+        self.inlayer = nn.Linear(num_in, hidden_width)
+
+        self.layers = nn.ModuleList([nn.Linear(hidden_width, hidden_width) for i in (self.depth - 1)])
+
+        self.output = nn.Linear(hidden_width, num_classes)
+
+    def forward(self, x):
+        x = torch.tanh(self.inlayer(x))
+        for  i, layer in enumerate(self.layers):
+            x = torch.tanh(self.layers(x))
+
+        x = self.output(x)
+        return x
