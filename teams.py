ee0c93686fea22626255a8935c6ef1035fd6476b
Jason Rader <38091354+RaderJason@users.noreply.github.com>
2021-12-12 23:06:52 +0000

    Fixed minor bugs

--- teams.py
+++ teams.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-class base_conv(nn.Module):
+class FFNet(nn.Module):
     def __init__(self, num_in, num_classes, hidden_width, depth):
         super().__init__()
 
@@ -8,14 +8,14 @@ class base_conv(nn.Module):
 
         self.inlayer = nn.Linear(num_in, hidden_width)
 
-        self.layers = nn.ModuleList([nn.Linear(hidden_width, hidden_width) for i in (self.depth - 1)])
+        self.layers = nn.ModuleList([nn.Linear(hidden_width, hidden_width) for i in range(self.depth - 1)])
 
         self.output = nn.Linear(hidden_width, num_classes)
 
     def forward(self, x):
         x = torch.tanh(self.inlayer(x))
         for  i, layer in enumerate(self.layers):
-            x = torch.tanh(self.layers(x))
+            x = torch.tanh(layer(x))
 
         x = self.output(x)
         return x
