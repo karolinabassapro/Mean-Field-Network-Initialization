52902589d7dbceaa94ba681b577d20bac0be9327
karolinabassapro <95982869+karolinabassapro@users.noreply.github.com>
2022-01-02 10:25:22 +0100

    Delete plotting.py

--- plotting.py
+++
@@ -1,8 +0,0 @@
-from main import *
-
-plt.plot(x, accs_r, label="relu")
-plt.plot(x, accs_t, label="tanh")
-plt.plot(x, accs_ht, label="hard_tanh")
-plt.title("Accuracies for Different Inits")
-plt.legend()
-plt.show()
